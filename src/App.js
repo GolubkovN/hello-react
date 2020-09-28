5e217eeb578e6db1af02918c574c206a07a96c8c
Golubkov Nikita <nigolubkov@gmail.com>
2020-09-28 18:02:08 +0300

    add first react-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import TodoList from './todo/TodoList.js';
 function App() {
+  const todos = [
+    {id: 1, completed: false, title: `Купить хлеб`},
+    {id: 2, completed: false, title: `Купить cыр`},
+    {id: 3, completed: false, title: `Купить масло`},
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <h1>React Tutorial</h1>
+
+      <TodoList todos={todos} />
     </div>
-  );
+  )
 }
 
 export default App;
