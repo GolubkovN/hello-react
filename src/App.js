7c9df9e2b03c78ffaa8a68a4018adeb7d7af5664
Golubkov Nikita <nigolubkov@gmail.com>
2020-09-28 19:21:12 +0300

    add callback done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
+import { func } from 'prop-types';
 import React from 'react';
 import TodoList from './todo/TodoList.js';
+
 function App() {
-  const todos = [
+  const [todos, setTodos] = React.useState([
     {id: 1, completed: false, title: `Купить хлеб`},
     {id: 2, completed: false, title: `Купить cыр`},
     {id: 3, completed: false, title: `Купить масло`},
-  ];
+  ]);
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+        }
+        return todo;
+      })
+    )
+  }
 
   return (
     <div className='wrapper'>
       <h1>React Tutorial</h1>
 
-      <TodoList todos={todos} />
+      <TodoList todos={todos} onToggle={toggleTodo}/>
     </div>
   )
 }
